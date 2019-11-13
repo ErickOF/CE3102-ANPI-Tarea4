e4058461d73247ad9cc619c66783e804a61524cc
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-11-13 00:28:04 -0600

    Thomas Method was added

---
+++ thomas.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+
+# Thomas Algorithm
+def thomas(A, b):
+    return np.linalg.solve(A, b)
+
+
+if __name__ == '__main__':
+    print('Thomas method working, put your code below')
